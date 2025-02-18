cd8e318e97f0eca69747ab952e13f06003fd94d7
DerKaiser2023 <discordballs872@gmail.com>
2025-02-17 22:10:58 -0500

    Create boot_image.h

---
+++ ui/boot_image.h
@@ -0,0 +1,11 @@
+#ifndef BOOT_IMAGE_H
+#define BOOT_IMAGE_H
+
+#define IMAGE_WIDTH  320
+#define IMAGE_HEIGHT 240
+
+const unsigned int boot_image_data[IMAGE_WIDTH * IMAGE_HEIGHT] = {
+    // Replace with your converted pixel data
+};
+
+#endif
